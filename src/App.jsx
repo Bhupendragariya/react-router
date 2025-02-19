9c1e6e117a06353827d5a09366277fde30a1cf03
Bhupendragariya <164327138+Bhupendragariya@users.noreply.github.com>
2025-02-19 11:45:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './component/Header/Header'
 import Footer from './component/Footer/Footer'
+import About from "./component/About/About";
+
 import { Outlet } from 'react-router-dom'
 
 function App() {
